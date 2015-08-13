ab2560d7825ef105adad498dc4ddc99e04e7c8a5
Fredmental <supervemon@hotmail.com>
2015-08-13 12:39:47 -0230

    Update BusterPelletController.cs

--- BusterPelletController.cs
+++ BusterPelletController.cs
@@ -25,9 +25,30 @@ public class BusterPelletController : MonoBehaviour
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, GetComponent<Rigidbody2D> ().velocity.y);
 	}
 
-    // Destroy pellet when it has a collision with anything. 
-	void OnCollisionEnter2D(Collision2D coll)
+	// Destroy enemy on contact with pellet. Enemies along with health system to be added. 
+	void OnTriggerEnter2D(Collider2D coll)
 	{
+		if (coll.tag == "Enemy")
+		{
+			Destroy (gameObject);
+		}
+
+	}
+
+	// When object is destroyed, decreased the pellet counter so there can only be 3 pellets on screen. 
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		if (coll.tag == "Enemy")
+		{
+			player.pelletCounter--;
+		}
+	}
+
+	// When Buster pellet is offscreen, destroy it so it doesn't mess with performance. Also decrease pellet counter
+	// so only 3 pellets are visible on screen.
+	void OnBecameInvisible()
+	{
+		player.pelletCounter--;
 		Destroy (gameObject);
 	}
 }
